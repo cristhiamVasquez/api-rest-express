4618874d0212b73e93eb061033a3def65739cdac
Cristhiam <cristhiam.vasquez@gmail.com>
2018-06-28 18:54:34 -0400

    variable de entorno heroku accesos db restringidos

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {} {
-    urlDB = 'mongodb://cafe-user:cafe123@ds061375.mlab.com:61375/cafe-db';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
