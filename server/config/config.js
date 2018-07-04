d7227f76de14052ff1d5267150ed6a5ef3739eb6
Cristhiam <cristhiam.vasquez@gmail.com>
2018-07-04 15:59:36 -0400

    Login normal - actualizacion de config.js

--- server/config/config.js
+++ server/config/config.js
@@ -15,12 +15,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ======================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-urlDB = 'mongodb://localhost:27017/cafe';
-//} else {} {
-//    urlDB = process.env.MONGO_URI;
-//    urlDB = 'mongodb://cafe-user:cafe123@ds061375.mlab.com:61375/cafe-db';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {} {
+    //    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb://cafe-user:cafe123@ds061375.mlab.com:61375/cafe-db';
+}
 process.env.URLDB = urlDB;
 
 // ======================
