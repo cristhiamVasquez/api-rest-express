b0ad7bf068833c89f69889c2ad76666a813eb8dd
Cristhiam <cristhiam.vasquez@gmail.com>
2018-07-31 15:30:56 -0400

    Google sign In

--- server/config/config.js
+++ server/config/config.js
@@ -1,58 +1,43 @@
-// ======================
-// -----PUERTO-----------
-// ======================
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
 
 
-// ======================
-// -----ENTORNO----------
-// ======================
+// ============================
+//  Entorno
+// ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
-// ======================
-// -----BASE DE DATOS----
-// ======================
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //    urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb://cafe-user:cafe123@ds061375.mlab.com:61375/cafe-db';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
-// ======================
-// Expiracion TOKEN
-// ======================
-// 60 segundos
-// 60 minutos
-// 24 horas
-// 30 dias
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
-
-
-
-// ======================
-// SEED autenticacion
-// ======================
-process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
-
-/*let seed;
-
-if (process.env.SEED === 'este-es-el-seed-desarrollo') {
-    seed = 'este-es-el-seed-desarrollo';
-} else {
-    seed = process.env.SEED_PROD;
-};
-
-process.env.SEED = seed;
-*/
-
-
-// ======================
-// Google ClienID
-// ======================
-process.env.CLIENT_ID = process.env.CLIENT_ID || '798208209837-ua7rlmun4jl6dlg1pu3a3aa650iu2l5q.apps.googleusercontent.com';
+// ============================
+//  Google Client ID
+// ============================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '798208209837-6teb14vrrvanqp3epgfif121oa6mscbu.apps.googleusercontent.com';
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,13 +1,17 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
+
+
 const Usuario = require('../models/usuario');
 
 const app = express();
+
 
 
 app.post('/login', (req, res) => {
@@ -16,40 +20,30 @@ app.post('/login', (req, res) => {
 
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
 
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
 
-        if (usuarioDB.estado === false) {
+        if (!usuarioDB) {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'El usuario se encuentra desactivado'
+                    message: '(Usuario) o contraseña incorrectos'
                 }
             });
-        } else {
+        }
 
-            if (err) {
-                return res.status(500).json({
-                    ok: false,
-                    err
-                });
-            };
 
-            if (!usuarioDB) {
-                return res.status(400).json({
-                    ok: false,
-                    err: {
-                        message: '(Usuario) y/o constraseña incorrectos'
-                    }
-                });
-            };
-
-            if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-                return res.status(400).json({
-                    ok: false,
-                    err: {
-                        message: 'Usuario y/o (constraseña) incorrectos'
-                    }
-                });
-            };
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
         }
 
         let token = jwt.sign({
@@ -62,11 +56,13 @@ app.post('/login', (req, res) => {
             token
         });
 
+
     });
 
 });
 
-//Configuraciones de Google
+
+// Configuraciones de Google
 async function verify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
@@ -75,30 +71,104 @@ async function verify(token) {
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
     const payload = ticket.getPayload();
-    //console.log(JSON.stringify(payload, undefined, 2));
-    //const userid = payload['sub'];
 
-
-    /*console.log(payload.name);
-    console.log(payload.email);
-    console.log(payload.picture);*/
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
 
 }
 
 
-app.post('/google', (req, res) => {
-
+app.post('/google', async(req, res) => {
 
     let token = req.body.idtoken;
 
-    verify(token);
+    let googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                err: e
+            });
+        });
 
-    res.json({
-        token
-    })
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar su autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token,
+                });
+
+            }
+
+        } else {
+            // Si el usuario no existe en nuestra base de datos
+            let usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                };
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token,
+                });
+
+
+            });
+
+        }
+
+
+    });
 
 
 });
+
+
 
 
 
